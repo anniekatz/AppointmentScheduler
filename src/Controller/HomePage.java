3eb9e7bbc0d4b09d34cc559486729919b5e34f3c
annie katz <akatz.dev@gmail.com>
2022-08-16 08:22:42 -0700

    Updated View

---
+++ src/Controller/HomePage.java
@@ -0,0 +1,9 @@
+package Controller;
+
+import javafx.scene.control.Button;
+
+public class HomePage {
+    public Button AppointmentsButton;
+    public Button CustomersButton;
+    public Button ReportsButton;
+}
