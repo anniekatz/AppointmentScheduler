15e6e6f470e48845fe81b117ca060cf8adc43619
annie katz <akatz.dev@gmail.com>
2022-08-16 10:24:03 -0700

    Updated view and language resources

--- src/Controller/LoginPage.java
+++ src/Controller/LoginPage.java
@@ -3,19 +3,26 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginPage implements Initializable {
     public Button LoginButton;
+    public Label LocaleLabel;
+    public Label LangLabel;
+    public TextField UserIDTextField;
+    public TextField PasswordTextField;
+    public Label TitleLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Initializing");
     }
 
-    public void OnButtonLogin(ActionEvent actionEvent) {
+    public void OnButtonLogin(ActionEvent event) {
         System.out.println("OnButtonLogin clicked");
     }
 }
