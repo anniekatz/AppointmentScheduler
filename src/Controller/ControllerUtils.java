6dc400463e492001b8005f3d84cd7e3c84aed91c
annie katz <akatz.dev@gmail.com>
2022-08-17 19:31:22 -0700

    Added Navigation Methods

--- src/Controller/ControllerUtils.java
+++ src/Controller/ControllerUtils.java
@@ -3,9 +3,9 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,7 +16,7 @@ public class ControllerUtils {
     // Navigate to another window upon button click
     public static void NavigateToWindow(ActionEvent event, String PageResource, String PageTitle) throws IOException {
 
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Stage stage = (Stage)((Button)event.getSource()).getScene().getWindow();
             Parent scene = FXMLLoader.load(Objects.requireNonNull(ControllerUtils.class.getResource(PageResource)));
             stage.setTitle(PageTitle);
             stage.setScene(new Scene(scene));
