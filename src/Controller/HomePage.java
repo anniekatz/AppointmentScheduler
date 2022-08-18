737eb03a12886c1f1b6bcd4b690d967a324b1242
annie katz <akatz.dev@gmail.com>
2022-08-17 17:14:47 -0700

    Updated view

--- src/Controller/HomePage.java
+++ src/Controller/HomePage.java
@@ -1,9 +1,12 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 public class HomePage {
     public Button AppointmentsButton;
     public Button CustomersButton;
     public Button ReportsButton;
+
+
 }
