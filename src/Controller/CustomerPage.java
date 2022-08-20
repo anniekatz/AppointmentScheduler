fcfda6a80b65df795e110511a89c433dfc7d8401
annie katz <akatz.dev@gmail.com>
2022-08-20 12:50:30 -0400

    Updated customer controller

--- src/Controller/CustomerPage.java
+++ src/Controller/CustomerPage.java
@@ -1,24 +1,65 @@
 package Controller;
 
+import Model.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 
 public class CustomerPage {
+    // Navigation buttons
     @FXML
     private Button ReportsButton;
-
     @FXML
     private Button AppointmentsButton;
+
+
+    // Customer TableView Variables
+    @FXML
+    private TableView<Customer> CustomerTable;
+    @FXML
+    private TableColumn<Customer, String> CustomerTableAddressColumn;
+    @FXML
+    private TableColumn<Customer, String> CustomerTableCountryColumn;
+    @FXML
+    private TableColumn<Customer, Integer> CustomerTableCustomerIDColumn;
+    @FXML
+    private TableColumn<Customer, Integer> CustomerTableDivisionColumn;
+    @FXML
+    private TableColumn<Customer, String> CustomerTableNameColumn;
+    @FXML
+    private TableColumn<Customer, String> CustomerTablePhoneColumn;
+    @FXML
+    private TableColumn<Customer, String> CustomerTablePostalCodeColumn;
+
+    // Creating/Updating/Deleting Form Variables
+    @FXML
+    private Button AddUpdateButton;
+    @FXML
+    private TextField AddressTextField;
+    @FXML
+    private ComboBox<?> CountryComboBox;
+    @FXML
+    private TextField CustomerIDTextField;
+    @FXML
+    private Button DeleteButton;
+    @FXML
+    private ComboBox<?> DivisionComboBox;
+    @FXML
+    private TextField NameTextField;
+    @FXML
+    private TextField PhoneTextField;
+    @FXML
+    private TextField PostalCodeTextField;
+
+
 
     // Navigation button methods
     @FXML
     void NavToAppointments(ActionEvent event) throws IOException {
         ControllerUtils.NavToAppointments(event);
     }
-
     @FXML
     void NavToReports(ActionEvent event) throws IOException {
         ControllerUtils.NavToReports(event);
